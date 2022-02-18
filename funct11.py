503fc7e1b7ccb2cef47121bcd2d800d46ab3f96e
Almukhamed Amitov <79891549+almuhkz@users.noreply.github.com>
2022-02-18 22:37:18 +0600

    z

--- funct11.py
+++ funct11.py
@@ -4,9 +4,8 @@ def ispalindrome(splitted):
     if(len(splitted)==1):
         result = False
         word = splitted[0]
-        if(len(word)%2!=0):
-            mid = math.floor(len(word)/2)
-            for i in range(mid):
+        mid = math.floor(len(word)/2)
+        for i in range(mid):
                 if(word[i]==word[-i-1]):
                     result = True
         return result
